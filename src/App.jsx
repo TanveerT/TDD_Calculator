ef5bd0a82ebc92f6dae5036717133b3d65d03287
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2025-08-13 12:24:56 +0530

    creating front end ui in react to calculate sum

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { add } from "./utils/add";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [inputValue, setInputValue] = useState("");
+	const [sum, setSum] = useState(null);
+	const [error, setError] = useState("");
+	const handleSum = () => {
+		const list = inputValue.split(",");
+		try {
+			let totalSum = add(list);
+			setSum(totalSum);
+			setError("");
+			setInputValue("");
+		} catch (e) {
+			console.log("e", e.message);
+			setSum(null);
+			setError(e.message);
+		}
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// const add = (list) => {
+	// 	let total = 0;
+	// 	list.forEach((element) => {
+	// 		let number = parseInt(element);
+	// 		if (!isNaN(number)) {
+	// 			if (number < 0) {
+	// 				throw new Error("Negative value not allowed");
+	// 			} else {
+	// 				total += number;
+	// 			}
+	// 		} else {
+	// 			throw new Error("Specail character not allowed");
+	// 		}
+	// 	});
+	// 	return total;
+	// };
+
+	return (
+		<>
+			<h1>Incubyte Frontend assesment</h1>
+			<div className='card'>
+				<input
+					type='text'
+					value={inputValue}
+					onChange={(e) => {
+						setInputValue(e.target.value);
+					}}
+					className='input-filed'
+					placeholder='Enter numbers seperated by , to calculate sum'
+				/>
+				<button className='add-button' onClick={handleSum}>
+					Add
+				</button>
+			</div>
+			<div className='result'>
+				{sum !== null && <div>Sum is {sum}</div>}
+				{error !== "" && <div>{error}</div>}
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
