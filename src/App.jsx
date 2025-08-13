7ce0b87bd7497459df9c279a501c0ba9a83d33cb
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2025-08-13 12:36:38 +0530

    commented code removed

--- src/App.jsx
+++ src/App.jsx
@@ -22,23 +22,6 @@ function App() {
 		}
 	};
 
-	// const add = (list) => {
-	// 	let total = 0;
-	// 	list.forEach((element) => {
-	// 		let number = parseInt(element);
-	// 		if (!isNaN(number)) {
-	// 			if (number < 0) {
-	// 				throw new Error("Negative value not allowed");
-	// 			} else {
-	// 				total += number;
-	// 			}
-	// 		} else {
-	// 			throw new Error("Specail character not allowed");
-	// 		}
-	// 	});
-	// 	return total;
-	// };
-
 	return (
 		<>
 			<h1>Incubyte Frontend assesment</h1>
